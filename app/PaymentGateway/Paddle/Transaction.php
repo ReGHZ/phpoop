b65bc49598a51ea804f6600fd902043da4e64de0
Areghz <reifanarj@gmail.com>
2023-07-25 04:42:50 +0700

    Namespace

---
+++ app/PaymentGateway/Paddle/Transaction.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PaymentGateway\Paddle;
+
+use Notification\Email;
+
+class Transaction
+{
+    //use "\" to use build in function php
+    public function __construct()
+    {
+        var_dump(new Email());
+    }
+}
--- app/public/index.php
+++ app/public/index.php
@@ -1,4 +1,14 @@
 <?php
 
+require_once '../Notification/Email.php';
+require_once '../Paymentgateway/Paddle/CustomerProfile.php';
+require_once '../Paymentgateway/Paddle/Transaction.php';
+require_once '../Paymentgateway/Stripe/Transaction.php';
 
-phpinfo();
+use Paymentgateway\Stripe\Transaction as StripeTransaction;
+use Paymentgateway\Paddle\{Transaction, CustomerProfile};
+//or
+use Paymentgateway\Paddle;
+
+var_dump(new StripeTransaction());
+var_dump(new Transaction());
